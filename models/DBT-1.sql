7ecd38a373e5973d36ba38716fb46238d0e867eb
victorhuskey1@gmail.com <victorhuskey1@gmail.com>
2024-11-06 00:43:15 +0000

    commit

--- models/DBT-1.sql
+++ models/DBT-1.sql
@@ -1,3 +1,15 @@
-{{ config(schema='CUSTOMER_DATA') }}
+{{ config(
+    schema='DBT_CLOUD',
+    materialized='view'  
+) }}
 
-select * from CUSTOMERS
+with source_data as (
+    select *
+    from {{ source('CUSTOMER_DATA', 'CUSTOMERS') }}
+)
+
+select *
+from source_data
+
+-- Uncomment the line below to remove records with null `id` values
+-- where id is not null
